d7e3d81da15d6b438b127589ec2f900c072b141c
robibubble <37271458+robibubble@users.noreply.github.com>
2018-12-10 23:44:11 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mah_discord_bot
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        { 
+
+            Console.WriteLine(Utilities.GetAlert("test"));
+        }
+    }
+}
---
+++ Utilities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace mah_discord_bot
+{
+    class Utilities
+    {
+
+        static Utilities()
+        {
+            string json = File.ReadAllText("SystemLang\\alerts.json");
+            var data = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+        }
+
+        public static string GetAlert(string key)
+        {
+            return key;
+        }
+    }
+}
