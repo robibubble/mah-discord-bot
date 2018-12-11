8c81b961d86eea524d57f9899a3bacc0661de856
robibubble <37271458+robibubble@users.noreply.github.com>
2018-12-11 21:31:33 +0200

    Bot Starting Up

--- Program.cs
+++ Program.cs
@@ -1,20 +1,48 @@
-﻿using System;
+﻿using Discord.WebSocket;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
 
 namespace mah_discord_bot
 {
     class Program
     {
 
-        static void Main(string[] args)
-        { 
+        DiscordSocketClient _client;
+        CommandHandler _cmdHandler;
 
-            Console.WriteLine("token: " + Config.bot.token);
-            Console.WriteLine("cmdPrefix: " + Config.bot.cmdPrefix);
-            Console.ReadLine();
+        //Skip Main, Starts Async Instead
+        static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult();
+
+        public async Task StartAsync()
+        {
+            if (Config.bot.token == null || Config.bot.token == "")
+            {
+                return;
+            }
+
+            _client = new DiscordSocketClient(new DiscordSocketConfig{
+                LogLevel = LogSeverity.Verbose
+            });
+
+            _client.Log += Log;
+
+            await _client.LoginAsync(TokenType.Bot, Config.bot.token);
+            await _client.StartAsync();
+
+            _cmdHandler = new CommandHandler();
+            await _cmdHandler.AsyncInit(_client);
+            await Task.Delay(-1);
+
+        }
+
+        private async Task Log(LogMessage message)
+        {
+            Console.WriteLine(message.Message);
         }
     }
 }
