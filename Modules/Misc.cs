6ee99b9919c8ce0f28d2354278bc26d2126bc7a0
robibubble <37271458+robibubble@users.noreply.github.com>
2018-12-11 22:23:57 +0200

    Add first command

---
+++ Modules/Misc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace mah_discord_bot.Modules
+{
+
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+      
+        [Command("SayImportant")]
+        public async Task SayImportant([Remainder]string message)
+        {
+            var embed = new EmbedBuilder();
+
+            embed.WithTitle(Context.User.Username + "Has Something Important to say...");
+            embed.WithDescription(message);
+            embed.WithColor(new Color(Color.Red.RawValue));
+
+            await Context.Channel.SendMessageAsync("", false, embed);
+
+        }
+    }
+}
