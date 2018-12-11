39d650c90552bc933cedae5e9523b7191eafb09d
robibubble <37271458+robibubble@users.noreply.github.com>
2018-12-11 23:24:51 +0200

    add some more commands

--- Modules/Misc.cs
+++ Modules/Misc.cs
@@ -12,14 +12,39 @@ namespace mah_discord_bot.Modules
     public class Misc : ModuleBase<SocketCommandContext>
     {
       
-        [Command("SayImportant")]
-        public async Task SayImportant([Remainder]string message)
+        [Command("say")]
+        public async Task say([Remainder]string message)
         {
             var embed = new EmbedBuilder();
 
-            embed.WithTitle(Context.User.Username + "Has Something Important to say...");
-            embed.WithDescription(message);
-            embed.WithColor(new Color(Color.Red.RawValue));
+            if (message == "hi" && message == "hi guys")
+            {
+                embed.WithTitle(Context.User.Username + " Says Hi ");
+                embed.WithThumbnailUrl("https://pbs.twimg.com/media/Bnnn6C8IEAA9rhs.jpg:large");
+            }
+            else
+            {
+                embed.WithTitle(Context.User.Username + " Says...");
+                embed.WithDescription(message);
+                embed.WithColor(new Color(Color.DarkBlue.RawValue));
+            }
+
+            await Context.Channel.SendMessageAsync("", false, embed);
+        }
+
+        [Command("random")]
+        public async Task random([Remainder]string message)
+        {
+            string[] options = message.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Random randNr = new Random();
+            string slection = options[randNr.Next(0, options.Length)];
+
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Choise for " + Context.User.Username);
+            embed.WithDescription(slection);
+            embed.WithColor(new Color(Color.DarkGreen.RawValue));
+            //embed.WithThumbnailUrl("https://pbs.twimg.com/media/Bnnn6C8IEAA9rhs.jpg:large");
 
             await Context.Channel.SendMessageAsync("", false, embed);
 
